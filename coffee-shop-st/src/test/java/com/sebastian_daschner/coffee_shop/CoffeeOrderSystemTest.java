f7cccc95cf82f3c60fdd0386896874b2c9265958
Sebastian Daschner <git@sebastian-daschner.de>
2018-04-10 07:42:35 +0530

    completed testing-1
    This reverts commit 030b9592a9cb5c67617d30bfd7d580128dca15bb.

--- coffee-shop-st/src/test/java/com/sebastian_daschner/coffee_shop/CoffeeOrderSystemTest.java
+++ coffee-shop-st/src/test/java/com/sebastian_daschner/coffee_shop/CoffeeOrderSystemTest.java
@@ -1,15 +1,65 @@
 package com.sebastian_daschner.coffee_shop;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CoffeeOrderSystemTest {
 
+    private CoffeeOrderSystem coffeeOrderSystem;
+    private ProcessorSystem processorSystem;
+
+    @BeforeEach
+    void setUp() {
+        coffeeOrderSystem = new CoffeeOrderSystem();
+        processorSystem = new ProcessorSystem();
+    }
+
     @Test
     void createVerifyOrder() {
+        List<URI> originalOrders = coffeeOrderSystem.getOrders();
+
+        Order order = new Order("Espresso", "Colombia");
+        URI orderUri = coffeeOrderSystem.createOrder(order);
+
+        Order loadedOrder = coffeeOrderSystem.getOrder(orderUri);
+        assertThat(loadedOrder).isEqualToComparingOnlyGivenFields(order, "type", "origin");
+
+        assertThat(coffeeOrderSystem.getOrders()).hasSize(originalOrders.size() + 1);
     }
 
     @Test
     void createOrderCheckStatusUpdate() {
+        Order order = new Order("Espresso", "Colombia");
+        URI orderUri = coffeeOrderSystem.createOrder(order);
+
+        processorSystem.answerForOrder(orderUri, "PREPARING");
+
+        Order loadedOrder = coffeeOrderSystem.getOrder(orderUri);
+        assertThat(loadedOrder).isEqualToComparingOnlyGivenFields(order, "type", "origin");
+
+        loadedOrder = waitForProcessAndGet(orderUri, "PREPARING");
+        assertThat(loadedOrder.getStatus()).isEqualTo("Preparing");
+
+        processorSystem.answerForOrder(orderUri, "FINISHED");
+
+        loadedOrder = waitForProcessAndGet(orderUri, "FINISHED");
+        assertThat(loadedOrder.getStatus()).isEqualTo("Finished");
+    }
+
+    private Order waitForProcessAndGet(URI orderUri, String requestedStatus) {
+        processorSystem.waitForInvocation(orderUri, requestedStatus);
+        return coffeeOrderSystem.getOrder(orderUri);
+    }
+
+    @AfterEach
+    void reset() {
+        processorSystem.reset();
     }
 
 }
