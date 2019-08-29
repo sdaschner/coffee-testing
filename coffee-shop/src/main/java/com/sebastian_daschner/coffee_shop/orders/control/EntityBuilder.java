33ac51c517cdd9b49c4cb3135cf09d85266e08c1
Sebastian Daschner <git@sebastian-daschner.de>
2019-08-29 08:55:31 +0200

    updated readme, scripts, and minor changes

--- coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/orders/control/EntityBuilder.java
+++ coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/orders/control/EntityBuilder.java
@@ -54,7 +54,6 @@ public class EntityBuilder {
         return Json.createObjectBuilder()
                 .add("_links", Json.createObjectBuilder()
                         .add("types", typesUri.toString()))
-                .add("Привет", "Питер")
                 .add("_actions", Json.createObjectBuilder()
                         .add("order-coffee", Json.createObjectBuilder()
                                 .add("method", "POST")
