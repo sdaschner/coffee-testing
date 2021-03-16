fb0f69f6c4f423fb2e5e47f29dcf5dbc16cf9437
Sebastian Daschner <git@sebastian-daschner.de>
2021-03-16 11:32:37 +0100

    minor changes

--- coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/Health.java
+++ coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/Health.java
@@ -14,7 +14,7 @@ public class Health implements HealthCheck {
     public HealthCheckResponse call() {
         return HealthCheckResponse.named("coffee-shop")
                 .up()
-                .withData("version", "3")
+                .withData("version", "4")
                 .build();
     }
 
