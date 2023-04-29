bfd5f2d19c0e1d087a025e759311d5f079202fc6
Sebastian Daschner <git@sebastian-daschner.de>
2023-04-29 18:07:07 +0200

    updated systemtest startup script

--- systemtest-run-env.sh
+++ systemtest-run-env.sh
@@ -10,7 +10,8 @@ docker run -d --rm \
   -p 5432:5432 \
   -e POSTGRES_USER=postgres \
   -e POSTGRES_PASSWORD=postgres \
-  postgres:9.5
+  -v $(pwd)/src/main/resources/scripts:/scripts:ro \
+  postgres:15.2
 
 docker run -d --rm \
   --name barista \
@@ -18,11 +19,23 @@ docker run -d --rm \
   -p 8002:8080 \
   rodolpheche/wiremock:2.6.0
 
-# wait for db startup
-sleep 5
+echo 'waiting for db startup'
+until docker exec coffee-shop-db pg_isready -h localhost > /dev/null; do
+  sleep 0.5
+done;
 
+echo 'creating schema'
+docker exec coffee-shop-db psql -U postgres -f /scripts/schema.sql
+echo 'loading data into database'
+docker exec coffee-shop-db psql -U postgres -f /scripts/load-data.sql
+
+echo 'starting coffee-shop'
 docker run -d --rm \
   --name coffee-shop \
   --network dkrnet \
   -p 8001:8080 \
   coffee-shop
+
+until [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8001/q/health)" == "200" ]]; do
+  sleep 0.5
+done;
